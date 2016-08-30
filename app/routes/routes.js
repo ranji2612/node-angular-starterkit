cba791ff036cdcf2fd6c7cb175d553e6115dc540
Ranjith Kumar <ranji2612@gmail.com>
2016-08-29 18:11:32 -0700

    Removing unnecessary

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,22 +1,5 @@
-
-//For file handling
-var fs = require('fs')
-var execSync = require('child_process').execSync;
-
-
 module.exports = function(app, passport) {
-	//Upload - To upload to CUNIX server
-   // TO render the upload page
-    app.post('/run', function(req, res){
-        console.log(req.body);
-        code = execSync('echo '+req.body.data+' > ./replPrg.plt; ./plt ./replPrg.plt');
-        res.json({"code":"success","name":"replPrg"});
-    });
-    // Load the home page for others apart from api
-    app.get('/*', function(req, res){
-		
-		res.sendfile('public/index.html');	
-		
-	});
-	
+  app.get('/*', function(req, res){
+    res.sendfile('public/index.html');
+  });
 };
